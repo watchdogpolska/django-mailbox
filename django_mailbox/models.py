550725793605660c66927ae72582331eb312070c
Adam Dobrawy <naczelnik@jawnosc.tk>
2018-07-15 21:36:30 +0200

    Fix PY3 compatibility

--- django_mailbox/models.py
+++ django_mailbox/models.py
@@ -390,7 +390,10 @@ class Mailbox(models.Model):
         if settings['compress_original_message']:
             with NamedTemporaryFile(suffix=".eml.gz") as fp_tmp:
                 with gzip.GzipFile(fileobj=fp_tmp, mode="w") as fp:
-                    fp.write(message.as_string().decode('utf-8','replace').encode('utf-8'))
+                    if six.PY3:
+                        fp.write(message.as_bytes())
+                    else:
+                        fp.write(message.as_string().decode('utf-8','replace').encode('utf-8'))
                 msg.eml.save(
                     "%s.eml.gz" % (uuid.uuid4(), ),
                     File(fp_tmp),
